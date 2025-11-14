f3f086055c376b409db0ce7f9ea4fc16c4f2792a
Martin Randa <contact@rnd195.anonaddy.com>
2025-11-14 16:59:16 +0100

    Add matrix multiplication blog post

--- CLEANUP.py
+++ CLEANUP.py
@@ -1,7 +1,7 @@
 import glob
 
 
-def rewriter(file_path: str, search_list: list) -> None:
+def rewriter(file_path: str, search_list: list, allow_list: list) -> None:
     """Rewrites lines matching certain criteria by commenting them out in html"""
     print("-" * 50, "\nProcessing file:", file_path)
 
@@ -14,11 +14,19 @@ def rewriter(file_path: str, search_list: list) -> None:
     # Check every line if it contains a particular substring and apply html comment if it does
     for key, line in enumerate(inputs):
         if any(string in line for string in search_list) and "<!--" not in line:
-            print(f"Rewriting line {key}:", line)
+            if any(string in line for string in allow_list):
+                print(f"NOTE: Skipping line {key} because it's allowed")
+                continue
+            print(f"ACTION: Rewriting line {key}:", line)
             # Add '<!--' to the start and '-->' after the first '</script>'
             commented_line = "<!--" + line.rstrip().replace("</script>", "</script> -->\n", 1)
             outputs[key] = commented_line
             line_found = True
+            continue
+        # Let me know if there are any other external connections
+        if any(string in line for string in ["http://", "https://"]) and "<script" in line:
+            print(f"WARN: Line {key} contains an external script that is not tracked:", line)
+            
 
     # Rewrite the file if any of the lines meet the criteria
     if line_found:
@@ -30,7 +38,7 @@ def rewriter(file_path: str, search_list: list) -> None:
 
 files = glob.glob(".\\docs\\**\\*.html", recursive=True)
 # If these substrings are present in a line, comment the line out
-search_scripts = [
+not_allowed = [
     "https://cdnjs.cloudflare.com/polyfill",
     "https://cdn.jsdelivr.net/npm/mathjax",
     "https://cdnjs.cloudflare.com/ajax/libs/",
@@ -38,8 +46,13 @@ search_scripts = [
     "https://cdn.jsdelivr.net/npm/",
     """type="application/javascript">define('jquery'"""
 ]
+allowed = [
+    # Bokeh-related
+    "tabler/tabler-icons/",
+    "MathJaxProvider"
+]
 
 for f in files:
-    rewriter(f, search_scripts)
+    rewriter(f, not_allowed, allowed)
 
 print("\nFinished cleanup")
